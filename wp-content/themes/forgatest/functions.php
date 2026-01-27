3b41e5c8764e7e905b3e3d45ba43b36394f82652
Branislav Stojanovic <banestojanovics@gmail.com>
2026-01-27 16:08:14 +0100

    init commit

---
+++ wp-content/themes/forgatest/functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * ForgaTest functions and definitions.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Forga
+ * @subpackage ForgaCommerce
+ * @since ForgaCommerce 1.0
+ */
+
+
